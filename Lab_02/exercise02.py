3ab7848c462852ceb98595072f3ab9f0b5f185e2
AlexRTC1997 <aturpoco@unsa.edu.pe>
2024-05-04 23:48:22 -0500

    feature: Lab 02 Exercise 02 done

--- Lab_02/exercise02.py
+++ Lab_02/exercise02.py
@@ -2,3 +2,37 @@
 # tiene una velocidad inicial "Vi" y una velocidad final "Vf". Escriba un código
 # que determine la fuerza que describe el móvil al momento de realizar
 # el cambio de velocidad y grafique el proceso
+import matplotlib.pyplot as plt
+
+option = None
+
+while option != 2:
+    option = int(input('[1] F [2] Exit: '))
+
+    if option == 1:
+        mass = float(input('> Mass: '))
+        time = float(input('> Time: '))
+        initial_speed = float(input('> Initial speed: '))
+        final_speed = float(input('> Final speed: '))
+
+        acceleration = (final_speed - initial_speed) / time
+        force = mass * acceleration
+
+        # Output
+        print('\n[Result]: \n')
+        print(f'> Force: {force}N')
+
+        # Graph
+        times = range(int(time) + 1)
+        speeds = [(initial_speed + (final_speed - initial_speed) * t / time)
+                  for t in times]
+
+        plt.plot(times, speeds)
+        plt.title('Speed change graph')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Speed (m/s)')
+        plt.grid(True)
+        plt.show()
+
+    elif option == 2:
+        print('> Program finished...\n')
