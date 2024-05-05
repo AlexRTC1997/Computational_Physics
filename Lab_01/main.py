3987f030a60b64dd62c78036b9bd9f76baa68d5b
AlexRTC1997 <aturpoco@unsa.edu.pe>
2024-05-04 21:30:15 -0500

    feature: MRU completed

--- Lab_01/main.py
+++ Lab_01/main.py
@@ -4,34 +4,42 @@ if __name__ == '__main__':
     option = None
 
     while option != 3:
-        option = int(input('[1]MRU [2]MRUV [3]Exit: '))
+        option = int(input('[1] MRU [2] MRUV [3] Exit: '))
         if option == 1:
             mru_option = None
 
             while mru_option != 4:
-                mru_option = int(input('[1]Δx [2]Vx [3]Δt [4]Back: '))
+                mru_option = int(input('\n[1] Δx [2] V [3] Δt [4] Back: '))
+                print()
                 if mru_option == 1:
-                    # Δx = VxΔt
-                    Vx = float(input('Vx: '))
-                    Δt = float(input('Δt: '))
-                    Δx = Vx * Δt
-                    print(f'Δx = {Δx}')
+                    V = float(input('> V: '))
+                    Δt = float(input('> Δt: '))
+
+                    print('> Δx = V x Δt')
+                    print(f'  Δx = {V} x {Δt}')
+                    print(f'  Δx = {V * Δt}')
                 elif mru_option == 2:
-                    # Δt = Δx / Vx
-                    Δx = float(input('Δx: '))
-                    Vx = float(input('Vx: '))
-                    while Vx == 0:
-                        Vx = float(input('Vx: '))
-                    Δt = Δx / Vx
-                    print(f'Δt = {Δt}')
+                    print('Note: V != 0')
+                    Δx = float(input('> Δx: '))
+                    V = float(input('> V: '))
+
+                    while V == 0:
+                        V = float(input('V: '))
+
+                    print('> Δt = Δx / V')
+                    print(f'  Δt = {Δx} / {V}')
+                    print(f'  Δt = {Δx / V}')
                 elif mru_option == 3:
-                    # Vx = Δx / Δt
-                    Δx = float(input('Δx: '))
-                    Δt = float(input('Δt: '))
+                    print('Note: Δt != 0')
+                    Δx = float(input('> Δx: '))
+                    Δt = float(input('> Δt: '))
+
                     while Δt == 0:
                         Δt = float(input('Δt: '))
-                    Vx = Δx / Δt
-                    print(f'Vx = {Vx}')
+
+                    print('> V = Δx / Δt')
+                    print(f'  V = {Δx} / {Δt}')
+                    print(f'  V = {Δx / Δt}')
                 elif mru_option == 4:
                     break
 
@@ -40,6 +48,6 @@ if __name__ == '__main__':
             # Plus Graph
             print('Running option 2')
 
-        elif option == 3:
             # Vf = ViΔt + αΔt
-            print('Exiting program')
+        elif option == 3:
+            print('\n> Program finished...\n')
