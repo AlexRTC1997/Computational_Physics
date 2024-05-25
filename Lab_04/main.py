efeea77a4132dec59b4310596ea9d7364de98bb7
AlexRTC1997 <aturpoco@unsa.edu.pe>
2024-05-25 08:30:01 -0500

    chore: Commenting code

--- Lab_04/main.py
+++ Lab_04/main.py
@@ -3,38 +3,38 @@ import matplotlib.pyplot as plt
 from magpylib.source.magnet import Cylinder, Box
 from magpylib import Collection, displaySystem
 
-# Create magnets
+# [1] Create magnets
 x1 = Box(mag=(0, 0, 600), dim=(3, 3, 3), pos=(-4, 0, 3))
 x2 = Cylinder(mag=(0, 0, 500), dim=(3, 5))
 
-# Create collection
+# [2] Create collection
 c = Collection(x1, x2)
 
-# Manipulate magnets individually
+# [3] Manipulate magnets individually
 x1.rotate(45, (0, 1, 0), anchor=(0, 0, 0))
 x2.move((5, 0, -4))
 
-# Manipulate collection
+# [4] Manipulate collection
 c.move((-2, 0, 0))
 
-# Calculate B-field on a grid
+# [5] Calculate B-field on a grid
 xs = np.linspace(-10, 10, 33)
 zs = np.linspace(-10, 10, 44)
 POS = np.array([(x, 0, z) for z in zs for x in xs])
 Bs = c.getB(POS).reshape(44, 33, 3)  # VECTORIZED
 
-# Create figure
+# [6] Create figure
 fig = plt.figure(figsize=(9, 5))
 ax1 = fig.add_subplot(121, projection='3d')  # 3D-axis
 ax2 = fig.add_subplot(122)  # 2D-axis
 
-# Display system geometry on ax1
+# [7] Display system geometry on ax1
 displaySystem(c, subplotAx=ax1, suppress=True)
 
-# Display field in xz-plane using matplotlib
+# [8] Display field in xz-plane using matplotlib
 X, Z = np.meshgrid(xs, zs)
 U, V = Bs[:, :, 0], Bs[:, :, 2]
 ax2.streamplot(X, Z, U, V, color=np.log(U ** 2 + V ** 2))
 
-# Display plot
+# [9] Display plot
 plt.show()
